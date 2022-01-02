315c40e1a4a2f1acfb27befecc7c7194084c1f8c
Hunter-Dinan <hunter.dinan@my.jcu.edu.au>
2022-01-02 12:58:41 +1000

    Add base functionality for other views/screens

--- code/water_tracker.py
+++ code/water_tracker.py
@@ -6,6 +6,9 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 
 from daily_view import DailyView
 from weekly_view import WeeklyView
+from monthly_view import MonthlyView
+from yearly_view import YearlyView
+from social_view import SocialView
 
 
 class WaterTrackerApp(App):
@@ -14,12 +17,18 @@ class WaterTrackerApp(App):
         """Build Kivy app and open Daily View."""
         self.title = "Water Tracker"
 
-        Builder.load_file('daily_view.kv')
-        Builder.load_file('weekly_view.kv')
+        Builder.load_file('kv-files/daily_view.kv')
+        Builder.load_file('kv-files/weekly_view.kv')
+        Builder.load_file('kv-files/monthly_view.kv')
+        Builder.load_file('kv-files/yearly_view.kv')
+        Builder.load_file('kv-files/social_view.kv')
 
         screen_manager = ScreenManager()
         screen_manager.add_widget(DailyView(name='DailyView'))
         screen_manager.add_widget(WeeklyView(name='WeeklyView'))
+        screen_manager.add_widget(MonthlyView(name='MonthlyView'))
+        screen_manager.add_widget(YearlyView(name='YearlyView'))
+        screen_manager.add_widget(SocialView(name='SocialView'))
         return screen_manager
 
 
---
+++ code/yearly_view.py
@@ -0,0 +1,10 @@
+"""Yearly View of Water Tracker program."""
+from kivy.uix.screenmanager import Screen
+
+
+class YearlyView(Screen):
+    """Yearly View."""
+    # def __init__(self):
+    #     """Initialise Daily View."""
+    #     self.water = 0
+    pass
