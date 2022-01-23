1f247c0b1d4f9d6b75f387b1381c96dcace14ed2
Hunter <Hunter@daktech.com.au>
2022-01-23 15:10:20 +1000

    Add functionality to add water and view daily water intake

--- code/console_water_tracker.py
+++ code/console_water_tracker.py
@@ -1,5 +1,4 @@
-
-MENU = """Console Water Tracker
+MENU = """Console Water Tracker Menu
 (A)dd water
 (D)aily view
 (W)eekly view
@@ -8,13 +7,18 @@ MENU = """Console Water Tracker
 
 
 def main():
+    current_water_amount_litres = 0.0
+    required_daily_water_amount_litres = 3.5
+
     print(MENU)
     menu_input = input(">>> ").upper()
     while menu_input != "Q":
         if menu_input == "A":
-            print("A")
+            add_water_amount_litres = get_water_amount_litres(current_water_amount_litres,
+                                                              required_daily_water_amount_litres)
+            current_water_amount_litres += add_water_amount_litres
         elif menu_input == "D":
-            print("D")
+            display_daily_water_intake(current_water_amount_litres, required_daily_water_amount_litres)
         elif menu_input == "W":
             print("W")
         else:
@@ -22,6 +26,19 @@ def main():
         print(MENU)
         menu_input = input(">>> ").upper()
     print("Goodbye")
+
+
+def get_water_amount_litres(current_water_amount, required_daily_water_amount):
+    print("Current daily water intake: {}L , Required daily water intake: {}L".format(
+        current_water_amount, required_daily_water_amount))
+    print("Enter amount of water to add in litres:")
+    water_amount = float(input(">>> "))
+    return water_amount
+
+
+def display_daily_water_intake(current_water_amount, required_daily_water_amount):
+    print("Current daily water intake: {}L , Required daily water intake: {}L".format(
+        current_water_amount, required_daily_water_amount))
 
 
 main()
