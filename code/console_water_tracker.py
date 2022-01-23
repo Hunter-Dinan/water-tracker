a110bfdbb63546e0414480106dc8b188aeba8cc9
Hunter <Hunter@daktech.com.au>
2022-01-23 16:25:52 +1000

    Fix get_current_water_data function to check if data has the correct date

--- code/console_water_tracker.py
+++ code/console_water_tracker.py
@@ -8,19 +8,21 @@ MENU = """Console Water Tracker Menu
 """
 WATER_DATA_FILE = "daily_water_save.csv"
 REQUIRED_DAILY_WATER_AMOUNT_LITRES = 3.5
-CURRENT_DATE_INDEX = 0
+LATEST_DATA_INDEX = 0
+DATE_INDEX = 0
 WATER_AMOUNT_INDEX = 1
 
 
 def main():
     # Date format: YYYY-MM-DD
-    current_date = datetime.date.today()
+    current_date = str(datetime.date.today())
 
     # Daily water data format: ['YYYY-MM-DD,0.0,n', 'YYYY-MM-DD,0.0,n']
     daily_water_data = get_daily_water_data()
 
     # Current water data format: ['YYYY-MM-DD', '0.0', 'n']
-    current_water_data = get_current_water_data(daily_water_data)
+    current_water_data = get_current_water_data(current_date, daily_water_data)
+    print(current_water_data)
 
     current_water_amount_litres = current_water_data[WATER_AMOUNT_INDEX]
 
@@ -66,8 +68,12 @@ def get_daily_water_data():
     return daily_water_data
 
 
-def get_current_water_data(daily_water_data: list):
-    return daily_water_data[CURRENT_DATE_INDEX].split(',')
+def get_current_water_data(current_date, daily_water_data: list):
+    latest_water_data = daily_water_data[LATEST_DATA_INDEX].split(',')
+    if current_date == latest_water_data[DATE_INDEX]:
+        return latest_water_data
+    else:
+        return [current_date, 0.0, 'n']
 
 
 main()
